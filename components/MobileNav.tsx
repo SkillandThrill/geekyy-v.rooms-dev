443febab39a502e16227da876d787749e7e96490
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-05-14 00:05:43 +0530

    created the navbar and sidebar completely with the modern ui components

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Sheet,
     SheetContent,
@@ -10,10 +11,9 @@ import { sidebarLinks } from "@/constants"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
 
-
 const MobileNav = () => {
     const pathname = usePathname()
-return (
+  return (
     <section className='w-full max-w-[264px]'>
         <Sheet>
          <SheetTrigger asChild>
@@ -42,31 +42,32 @@ return (
             <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
                 <SheetClose asChild>
                     <section className="flex h-full flex-col gap-6 pt-16 text-white">
-                        {sidebarLinks.map((link)=>{
-                            const isActive =pathname === link.route || pathname.startsWith(link.route);
+                        {sidebarLinks.map((link) => {
+                            const isActive = pathname === link.route;
 
-                        return (
-                            <Link href={link.route}
-                                key={link.label}
-                                className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{
-                                'bg-blue-1':isActive,
+                            return (
+                                <SheetClose asChild key={link.route}>
+                                    <Link href={link.route}
+                               key={link.label}
+                               className={cn('flex gap-4 items-center p-4 rounded-lg w-full max-w-60',{
+                            'bg-blue-1':isActive,
                         })}
                             >
                             <Image
-                                src={link.imgurl}
-                                alt={link.label}
-                                width={24}
-                                height={24}
+                            src={link.imgurl}
+                            alt={link.label}
+                            width={20}
+                            height={20}
                             />
-                            <p className='text-lg font-semibold max-lg:hidden'>
-                                {link.label}
+                            <p className='font-semibold'>
+                            {link.label}
                             </p>
                             </Link>
-        )
-})}
+                            </SheetClose>
+                            )
+                            })}
 
                     </section>
-
                 </SheetClose>
             </div>
             </SheetContent>
