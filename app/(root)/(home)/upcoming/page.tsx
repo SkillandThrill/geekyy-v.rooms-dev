c120c6959425c0c383e9bf305ba54c3d30cfe1a9
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-05-25 19:52:49 +0530

    starting to create the upcoming meeting page where we can see all of the meeting that we scheduled.. we will be creating and implementing the same component in all three pages upcoming, previous and recording. lets create a reusable callMeeting list component which will render different set of cards.. for that created a customized hook into hooks folder named as useGetCalls.. in that created useState and useEffect hooks for displaying the meet details..whenever we call the hooks we use the component as use-client or use-server. we have created the meetingCard and proceeding to the callList component for adding the props to the component.. used a lot of logic into the callList and MeetingCard component to use it as a reusable component..just have to use the callList component with the type of either ended previous or recordings.

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import CallList from '@/components/CallList'
+import { type ClassValue } from 'clsx';
 
 const Upcoming = () => {
   return (
-    <section className=' flex size-full flex-col gap-10 text-white'>
-        <h1 className='text-3xl font-bold'>
-          Upcoming
-        </h1>
+    <section className='flex size-full flex-col gap-10 text-white'>
+        <CallList type="upcoming"/>
       </section>
   )
 }
