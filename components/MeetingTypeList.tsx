49a59f2a25382331c210673e7c0d0cf762b321f6
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-05-28 21:52:38 +0530

    Now only the last part of the project is pending and that is join the meeting when you have'nt organized the meeting. so moving forward with the final design and to utilize the we are going to create the new modal we will use the structure of the previous models to have a head-start and will add the title and the route as isJoiningMeeting and router.push(values.link)..to join the someone else's meeting we have to take a input which will be imported from the shadcn ui component as <Input/>> we give it a placeholder and the className just to add the styling and the onChange property to add a event with the call back function with setting the values..

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,8 @@ import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
 import { useToast } from "@/components/ui/use-toast"
 import { Textarea } from "./ui/textarea"
 import ReactDatePicker from 'react-datepicker'
+import { Input } from "@/components/ui/input"
+
 
 
 const MeetingTypeList = () => {
@@ -158,6 +160,24 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
         />
+
+        <MeetingModal 
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={()=>setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center "
+        buttonText="Join Meeting"
+        handleClick={()=> router.push(values.link)}
+        >
+            <Input
+                placeholder="Meeting link"
+                className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0 "
+                onChange={(e) =>setValues({ ...values,link:e.target.value})}
+            />
+        </MeetingModal>
+
+    
+
     </section>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         className='max-sm:size-10'
          />
          <p className='text-[26px] font-extrabold text-white max-sm:hidden'>
-          Yoom
+          V-MEET
          </p>
       </Link>
       <div className='flex-between gap-5'>
